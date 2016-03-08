f0e8ef20c25937de61419e790df28aee3352b5fe
envyus <dev2012testing@gmail.com>
2016-03-08 18:42:10 +0530

    Added Jquery function scripts.js file.

---
+++ source/javascripts/scripts.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+
+$('#toc').on("click", "li a", function(event){
+	var dataUnique = $(this).parent('li').attr('data-unique');
+	if($(window).width() > 767)
+	{
+	$('html, body').animate({
+      scrollTop: $('#'+dataUnique).offset().top
+    }, 600);
+	}
+	else
+	{
+		$('html, body').animate({
+      scrollTop: ($('#'+dataUnique).offset().top-$('#header').outerHeight())
+    }, 600);
+	return false;
+	}
+});
+
+
+$('#nav-toggle').click(function() {
+	if ($(this).hasClass("navBtnActive") ) 
+	{
+		$( "#bs-navbar" ).animate({ right: "-300px"}, 500 );
+		$( "#bs-navbar" ).removeClass('navShadow');
+		$(this).removeClass('navBtnActive');
+	} 
+	else 
+	{
+		$( "#bs-navbar" ).animate({ right: "0" }, 500 );
+		$( "#bs-navbar" ).addClass('navShadow');
+		$(this).addClass('navBtnActive');
+	}	
+});
+})
+
+function stickySidebar() {
+var headerHeight = $('#header').outerHeight();
+var footerHeight = $('.footer').outerHeight();
+var windowHeight = $(window).height();
+var scrollPosition = $(window).scrollTop();
+	if (scrollPosition >= headerHeight) {
+		$('.tocify-wrapper').css('position','fixed').css('height',(windowHeight-footerHeight));
+	}
+	else {
+		$('.tocify-wrapper').css('position','relative');
+	}
+		
+}
+ 
+
+$(document).scroll(function(){ 
+   stickySidebar();
+});
